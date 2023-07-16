972569880b9b164cbc2c83941f00911117e10e6b
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-16 22:01:37 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComputedSignalComponent } from './computed-signal/computed-signal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "computed-signal", component: ComputedSignalComponent },
+  { path: "**", component: ComputedSignalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
