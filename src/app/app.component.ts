7c7a703a68fbcc10fc2c986b3ed43af87ef74c5b
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-16 21:45:04 +0530

    Normal properties implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public firstName: string = "Sajed";
+  public lastName: string = "Shaikh";
+  public fullName: string = this.firstName + " " + this.lastName;
+
+  public changeFirstName(fName: string): void {
+    this.firstName = fName;
+  }
+
+  public changeLastName(lName: string): void {
+    this.lastName = lName;
+  }
 }
